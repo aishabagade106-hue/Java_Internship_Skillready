0e8d7af377306fd9882994f5e6b1b8b576e9576d
Aisha Bagade <aishabagade106@gmail.com>
2026-06-19 10:46:11 +0530

    Add files via upload

---
+++ data_type.java
@@ -0,0 +1,10 @@
+public class data_type {
+    public static void main(String[] args) {
+        int age =15;//Integer data type
+        float salary =25000.500f;//Float data type
+        double pi = 3.14156789;//Double data type
+        char grade = 'A';//Char data type
+        boolean pass = true;//Boolean data type 
+        String my_name = "Aisha";//String data type
+    }
+}
