cbb33ee7eba3be35292bbdad5592ff2bfd37f17a
Aisha Bagade <aishabagade106@gmail.com>
2026-06-19 10:23:53 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+//Java is a pure object-oriented programming language.
+//Java (compiler jvm java virtual machine) always follws the object
+//what do you mean by keyword 
+//keywords means registerd word
+//already asin the task
+//what is the rules of variable declaration or class declaration
+//1st rule starting of the var are always alphabet or underscore(_)
+//2nd number are not allowed when declaration starting of var
+//3rd space are not allowed declaration of var class name 
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA");
+    }
+}
