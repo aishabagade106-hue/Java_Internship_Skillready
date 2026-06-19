13d1d104b7dba4743f98540c131dcb5664fb1ea2
Aisha Bagade <aishabagade106@gmail.com>
2026-06-19 10:42:34 +0530

    Add files via upload

---
+++ rulesofvariable.java
@@ -0,0 +1,20 @@
+// 1) do not start a variables as a digit or number
+//for ex 123abc=10 1a=20;
+//2) starting of the variable always from alphabet or underscore
+//for example int_a=10 int data=20; int_1=100;
+// 3) there is no space in variable
+//space are not allowed in the variable declaration
+//string my name = "aisha"
+//string my_name ="aisha"
+class rulesofvariable{
+    public static void main(String[] args) {
+        int abc123=100;
+        int myROLLNumber=123;
+        int _date=12345;
+        System.out.println(abc123);
+        System.out.println(myROLLNumber);
+        System.out.println(_date);
+    }
+}
+    
+
