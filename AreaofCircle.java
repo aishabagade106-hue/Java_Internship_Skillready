084781466e91f7e367b62f131460069ab1419e70
Aisha Bagade <aishabagade106@gmail.com>
2026-06-26 20:14:17 +0530

    Add files via upload

---
+++ AreaofCircle.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class AreaofCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextDouble();
+        double area = 3.14 * radius * radius;
+        System.out.println("Area = " + area);
+        sc.close();
+    }
+
+}
