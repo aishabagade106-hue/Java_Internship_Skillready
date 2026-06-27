652293a2c90bd442ae4bc61063415075db206078
Aisha Bagade <aishabagade106@gmail.com>
2026-06-27 14:53:48 +0530

    Add files via upload

---
+++ AssignmentOP.java
@@ -0,0 +1,10 @@
+package Operator;
+//assign the values in variable
+//= += -= *= /= %=
+public class AssignmentOP {
+    public static void main(String[] args) {
+        int a=10;
+        a += 5;
+        System.out.println("value of a = " +a);//15
+    }
+}
