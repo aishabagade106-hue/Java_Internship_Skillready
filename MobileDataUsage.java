66985d7eaf9d3352b4781794e468b97c0fec75aa
Aisha Bagade <aishabagade106@gmail.com>
2026-07-07 20:56:08 +0530

    Add files via upload

---
+++ MobileDataUsage.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class MobileDataUsage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalData = sc.nextInt();
+        int usedData = sc.nextInt();
+        int remainingData = totalData - usedData;
+        System.out.println("Remaining Data =" + remainingData + "GB");
+    }
+    
+}
