1b7600272e03e45d7cde1803e3f210b9cb2c9781
Aisha Bagade <aishabagade106@gmail.com>
2026-07-07 20:57:33 +0530

    Add files via upload

---
+++ MonthlySavingsCalculator.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class MonthlySavingsCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int savings =5000;
+        int additionalSavings = sc.nextInt();
+        savings += additionalSavings;
+        System.out.println("Total Savings =" + savings);
+    }
+    
+}
