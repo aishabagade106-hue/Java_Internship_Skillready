847dea814da2aa7db4fc4e90a8b621636d95a71f
Aisha Bagade <aishabagade106@gmail.com>
2026-07-07 20:59:37 +0530

    Add files via upload

---
+++ EventRegistrationValidation.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class EventRegistrationValidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        boolean feePaid = sc.nextBoolean();
+        boolean registration =(age >= 18) && feePaid;
+        System.out.println("Registration Successful =" + registration);
+    }
+}
